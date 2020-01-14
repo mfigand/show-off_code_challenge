c99c6c8650ea010af1c299c438768be5d0b2e1dd
mfigand <m.figand@gmail.com>
2020-01-14 21:54:06 +0100

    Fix lintern warnings

--- spec/controllers/api/v1/ping_controller_spec.rb
+++ spec/controllers/api/v1/ping_controller_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::PingController, type: :controller do
-  describe "GET ping" do
-    it "check ping" do
+  describe 'GET ping' do
+    it 'check ping' do
       get :ping
       expect(response.body).to eq({ data: 'pong' }.to_json)
     end
