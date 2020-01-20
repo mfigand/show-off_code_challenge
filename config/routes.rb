dc62737543e4aff281f317c1ccc402bfbd6826a0
mfigand <m.figand@gmail.com>
2020-01-20 13:37:15 +0100

    Add home & login routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'api/v1/site#home'
+
   namespace :api do
     namespace :v1 do
+      root to: 'site#home'
+
       get 'ping', to: 'ping#ping', as: 'ping'
+
+      get 'login', to: 'sessions#new'
+      post 'login', to: 'sessions#create'
+      get 'welcome', to: 'sessions#welcome'
 
       resources :users do
         resources :widgets, only: %i[index show]
